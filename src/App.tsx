aee15dd0d0153308e3afdbdeaaee3436bc3e909c
OlgaOparyna <olka-rb@yandex.ru>
2023-04-11 15:07:32 +0300

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
+      <Button title={"Add to cart"} onClick={()=>{}}/>
+      <Button title={"disabled"} onClick={()=>{}} disabled/>
     </div>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import React, { FC, ReactNode } from "react";
+import classNames from "classnames";
+
+import styles from "./Button.module.scss";
+
+type ButtonProps = {
+    title: string | ReactNode;
+    onClick: () => void;
+    disabled?: boolean;
+    className?: string;
+};
+const Button: FC<ButtonProps> = ({
+                                     title,
+                                     onClick,
+                                     disabled,
+                                     className,
+                                 }) => {
+    return (
+        <div
+            onClick={disabled ? undefined : onClick}
+            className={classNames(styles.primaryButton, {
+                [styles.disabledButton]: disabled,
+            })}
+        >
+            {title}
+        </div>
+    );
+};
+export default Button;
